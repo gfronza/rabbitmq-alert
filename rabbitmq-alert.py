0e0b26581d73a03552404924daf56f5e303e110f
Kostas Milonas <milonas.ko@gmail.com>
2016-11-13 10:21:23 +0000

    adds support for config file

--- rabbitmq-alert.py
+++ rabbitmq-alert.py
@@ -1,27 +1,49 @@
+#! /usr/bin/python
 # -*- coding: utf-8 -*-
 from optparse import OptionParser
+from ConfigParser import ConfigParser
 import urllib2
 import json
 import time
+import os.path
 
 
 def setup_options():
     arguments = OptionParser()
+    arguments.add_option("-c", "--config-file", dest="config_file", help="Path of the configuration file", type="string", default=None)
     arguments.add_option("--host", dest="host", help="RabbitMQ API address", type="string", default="localhost")
     arguments.add_option("--port", dest="port", help="RabbitMQ API port", type="string", default="55672")
     arguments.add_option("--username", dest="username", help="RabbitMQ API username", type="string", default="rabbitmq")
     arguments.add_option("--password", dest="password", help="RabbitMQ API password", type="string", default="rabbitmq")
     arguments.add_option("--vhost", dest="vhost", help="Name of the vhost to inspect", type="string", default="celery")
     arguments.add_option("--queue", dest="queue", help="Name of the queue to inspect", type="string", default="celery")
+    arguments.add_option("--check-rate", dest="check_rate", help="Conditions check frequency, in seconds.", type="int", default=60)
 
     arguments.add_option("--ready-queue-size", dest="ready_queue_size", help="Size of Ready messages on the queue to alert as warning", type="int", default=0)
     arguments.add_option("--unacknowledged-queue-size", dest="unack_queue_size", help="Size of the Unacknowledged messages on the queue to alert as warning", type="int", default=0)
     arguments.add_option("--total-queue-size", dest="total_queue_size", help="Size of the Total messages on the queue to alert as warning", type="int", default=0)
 
-    arguments.add_option("--check-rate", dest="check_rate", help="Conditions check frequency, in seconds.", type="int", default=60)
     arguments.add_option("--email-to", dest="email_to", help="List of comma-separated email addresses to send notification to", type="string")
 
-    return arguments.parse_args()[0]
+    options = arguments.parse_args()[0]
+
+    if not options.config_file is None and os.path.isfile(options.config_file):
+        config = ConfigParser()
+        config.read(options.config_file)
+
+        options.host = get_config_file_option(config, "Server", "host")
+        options.port = get_config_file_option(config, "Server", "port")
+        options.username = get_config_file_option(config, "Server", "username")
+        options.password = get_config_file_option(config, "Server", "password")
+        options.vhost = get_config_file_option(config, "Server", "vhost")
+        options.queue = get_config_file_option(config, "Server", "queue")
+        options.check_rate = float(get_config_file_option(config, "Server", "check_rate"))
+        options.ready_queue_size = get_config_file_option(config, "Conditions", "ready_queue_size")
+        options.unack_queue_size = get_config_file_option(config, "Conditions", "unack_queue_size")
+        options.total_queue_size = get_config_file_option(config, "Conditions", "total_queue_size")
+        options.email_to = get_config_file_option(config, "Email", "to")
+
+    return options
 
 
 def send_notification(param, current_value):
@@ -67,6 +89,13 @@ def run_notification_sender():
         except urllib2.HTTPError, e:
             print "Error code %s hitting %s" % (e.code, url)
             exit(1)
+
+
+def get_config_file_option(config, section, option):
+    if config.has_section(section) and config.has_option(section, option):
+        return config.get(section, option)
+
+    return None
 
 
 if __name__ == '__main__':
