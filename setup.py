4b11479bc96baf2a142c4e67424e1f22839a559a
Kostas Milonas <milonas.ko@gmail.com>
2016-11-17 16:48:05 +0000

    prepares publishing to pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = "rabbitmq-alert",
+    packages = ["rabbitmq-alert"],
+    version = "1.0",
+    description = "Send notifications when predefined conditions are met",
+    author = "Germano Fronza, Kostas Milonas",
+    author_email = "germano.inf@gmail.com",
+    url = "https://github.com/gfronza/rabbitmq-alert",
+    download_url = "https://github.com/gfronza/rabbitmq-alert/tarball/1.0",
+    keywords = ["rabbitmq", "alert", "monitor"],
+    classifiers = []
+)
