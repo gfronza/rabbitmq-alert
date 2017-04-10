74a4895eb705762a5a8a6325fa74f64a8fff2fb7
Kostas Milonas <milonas.ko@gmail.com>
2017-04-10 23:30:52 +0300

    adds tests for sending notifications, node conditions

--- rabbitmqalert/test_rabbitmqalert.py
+++ rabbitmqalert/test_rabbitmqalert.py
@@ -7,17 +7,27 @@ import rabbitmqalert
 from collections import namedtuple
 
 class rabbitMqAlertTestCase(unittest.TestCase):
+    MockOptions = namedtuple(
+        "Foo",
+        "host, port, vhost, queue, ready_queue_size, unack_queue_size,\
+        total_queue_size, consumers_connected, nodes_running, node_memory_used,\
+        email_to, email_from, email_subject, email_server, slack_url,\
+        slack_channel, slack_username"
+    )
+
     def setUp(self):
         rabbitmqalert.send_notification_real = rabbitmqalert.send_notification
-    
+        rabbitmqalert.urllib2_real = rabbitmqalert.urllib2
+
     def tearDown(self):
         rabbitmqalert.send_notification = rabbitmqalert.send_notification_real
-        
+        rabbitmqalert.urllib2 = rabbitmqalert.urllib2_real
+
     def test_check_queue_conditions_messages_ready_not_exceeding_option(self):
         response = self.construct_response_queue()
         rabbitmqalert.send_request = mock.MagicMock(return_value=response)
 
-        options = self.construct_options()
+        options = self.construct_options_complete()
         rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
 
         rabbitmqalert.send_notification = mock.MagicMock()
@@ -30,7 +40,7 @@ class rabbitMqAlertTestCase(unittest.TestCase):
         response["messages_ready"] = 2
         rabbitmqalert.send_request = mock.MagicMock(return_value=response)
 
-        options = self.construct_options()
+        options = self.construct_options_complete()
         rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
 
         rabbitmqalert.send_notification = mock.MagicMock()
@@ -42,7 +52,7 @@ class rabbitMqAlertTestCase(unittest.TestCase):
         response = self.construct_response_queue()
         rabbitmqalert.send_request = mock.MagicMock(return_value=response)
 
-        options = self.construct_options()
+        options = self.construct_options_complete()
         rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
 
         rabbitmqalert.send_notification = mock.MagicMock()
@@ -55,7 +65,7 @@ class rabbitMqAlertTestCase(unittest.TestCase):
         response["messages_unacknowledged"] = 2
         rabbitmqalert.send_request = mock.MagicMock(return_value=response)
 
-        options = self.construct_options()
+        options = self.construct_options_complete()
         rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
 
         rabbitmqalert.send_notification = mock.MagicMock()
@@ -67,7 +77,7 @@ class rabbitMqAlertTestCase(unittest.TestCase):
         response = self.construct_response_queue()
         rabbitmqalert.send_request = mock.MagicMock(return_value=response)
 
-        options = self.construct_options()
+        options = self.construct_options_complete()
         rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
 
         rabbitmqalert.send_notification = mock.MagicMock()
@@ -80,7 +90,7 @@ class rabbitMqAlertTestCase(unittest.TestCase):
         response["messages"] = 2
         rabbitmqalert.send_request = mock.MagicMock(return_value=response)
 
-        options = self.construct_options()
+        options = self.construct_options_complete()
         rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
 
         rabbitmqalert.send_notification = mock.MagicMock()
@@ -93,7 +103,7 @@ class rabbitMqAlertTestCase(unittest.TestCase):
         response["consumers_connected"] = 2
         rabbitmqalert.send_request = mock.MagicMock(return_value=response)
 
-        options = self.construct_options()
+        options = self.construct_options_complete()
         rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
 
         rabbitmqalert.send_notification = mock.MagicMock()
@@ -107,7 +117,7 @@ class rabbitMqAlertTestCase(unittest.TestCase):
         response.pop("consumer_bar")
         rabbitmqalert.send_request = mock.MagicMock(return_value=response)
 
-        options = self.construct_options()
+        options = self.construct_options_complete()
         rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
 
         rabbitmqalert.send_notification = mock.MagicMock()
@@ -115,25 +125,99 @@ class rabbitMqAlertTestCase(unittest.TestCase):
 
         rabbitmqalert.send_notification.assert_called_once()
 
-    def test_send_notification_sends_email_when_email_to_option_exists(self):
-        options = self.construct_options()
+    def test_check_node_conditions_messages_nodes_running_exceeding_option(self):
+        response = self.construct_response_node()
+        rabbitmqalert.send_request = mock.MagicMock(return_value=response)
+
+        options = self.construct_options_complete()
         rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
-        
+
+        rabbitmqalert.send_notification = mock.MagicMock()
+        rabbitmqalert.check_node_conditions(options)
+
+        rabbitmqalert.send_notification.assert_not_called()
+
+    def test_check_node_conditions_messages_nodes_running_beneath_option(self):
+        response = self.construct_response_node()
+        response.pop()
+        response.pop()
+        rabbitmqalert.send_request = mock.MagicMock(return_value=response)
+
+        options = self.construct_options_complete()
+        rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
+
+        rabbitmqalert.send_notification = mock.MagicMock()
+        rabbitmqalert.check_node_conditions(options)
+
+        rabbitmqalert.send_notification.assert_called_once()
+
+    def test_check_node_conditions_messages_node_memory_exceeding_option(self):
+        response = self.construct_response_node()
+        response[0]["mem_used"] = 2000000
+        rabbitmqalert.send_request = mock.MagicMock(return_value=response)
+
+        options = self.construct_options_complete()
+        rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
+
+        rabbitmqalert.send_notification = mock.MagicMock()
+        rabbitmqalert.check_node_conditions(options)
+
+        rabbitmqalert.send_notification.assert_called_once()
+
+    def test_check_node_conditions_messages_node_memory_beneath_option(self):
+        response = self.construct_response_node()
+        rabbitmqalert.send_request = mock.MagicMock(return_value=response)
+
+        options = self.construct_options_complete()
+        rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
+
+        rabbitmqalert.send_notification = mock.MagicMock()
+        rabbitmqalert.check_node_conditions(options)
+
+        rabbitmqalert.send_notification.assert_not_called()
+
+    def test_send_notification_sends_email_when_email_to_is_set(self):
+        options = self.construct_options_complete()
+        rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
+
         rabbitmqalert.smtplib = mock.MagicMock()
         rabbitmqalert.send_notification(options, "")
         rabbitmqalert.urllib2 = mock.MagicMock()
-        
+
         rabbitmqalert.smtplib.SMTP().sendmail.assert_called_once()
-        
-    def construct_options(self):
-        Options = namedtuple(
-            "Foo",
-            "host, port, vhost, queue, ready_queue_size, unack_queue_size,\
-            total_queue_size, consumers_connected, nodes_running, node_memory_used,\
-            email_to, email_from, email_subject, email_server, slack_url,\
-            slack_channel, slack_username"
-        )
-        options = Options(
+
+    def test_send_notification_sends_email_when_email_to_not_set(self):
+        options = self.construct_options_missing_email()
+        rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
+
+        rabbitmqalert.smtplib = mock.MagicMock()
+        rabbitmqalert.send_notification(options, "")
+        rabbitmqalert.urllib2 = mock.MagicMock()
+
+        rabbitmqalert.smtplib.SMTP().sendmail.assert_not_called()
+
+    def test_send_notification_sends_to_slack_when_options_are_set(self):
+        options = self.construct_options_complete()
+        rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
+
+        rabbitmqalert.smtplib = mock.MagicMock()
+        rabbitmqalert.urllib2 = mock.MagicMock()
+        rabbitmqalert.send_notification(options, "")
+
+        rabbitmqalert.urllib2.urlopen.assert_called_once()
+
+    def test_send_notification_sends_to_slack_when_an_option_not_set(self):
+        options = self.construct_options_missing_slack()
+        rabbitmqalert.setup_options = mock.MagicMock(return_value=options)
+
+        rabbitmqalert.smtplib = mock.MagicMock()
+        rabbitmqalert.urllib2 = mock.MagicMock()
+        rabbitmqalert.send_notification(options, "")
+
+        rabbitmqalert.urllib2.urlopen.assert_not_called()
+
+    def construct_options_complete(self):
+        options = self.MockOptions(
             host="foo",
             port=1,
             vhost="foo",
@@ -155,6 +239,52 @@ class rabbitMqAlertTestCase(unittest.TestCase):
 
         return options
 
+    def construct_options_missing_email(self):
+        options = self.MockOptions(
+            host="foo",
+            port=1,
+            vhost="foo",
+            queue="foo",
+            ready_queue_size=1,
+            unack_queue_size=1,
+            total_queue_size=1,
+            consumers_connected=1,
+            nodes_running=1,
+            node_memory_used=1,
+            email_to=None,
+            email_from="bar@foobar.com",
+            email_subject="foo %s %s",
+            email_server="mail.foobar.com",
+            slack_url="http://foo.com",
+            slack_channel="channel",
+            slack_username="username"
+        )
+
+        return options
+
+    def construct_options_missing_slack(self):
+        options = self.MockOptions(
+            host="foo",
+            port=1,
+            vhost="foo",
+            queue="foo",
+            ready_queue_size=1,
+            unack_queue_size=1,
+            total_queue_size=1,
+            consumers_connected=1,
+            nodes_running=1,
+            node_memory_used=1,
+            email_to="foo@foobar.com",
+            email_from="bar@foobar.com",
+            email_subject="foo %s %s",
+            email_server="mail.foobar.com",
+            slack_url=None,
+            slack_channel="channel",
+            slack_username="username"
+        )
+
+        return options
+
     def construct_response_queue(self):
         return {
             "messages_ready": 0,
@@ -167,5 +297,12 @@ class rabbitMqAlertTestCase(unittest.TestCase):
             "consumer_foo": {},
             "consumer_bar": {}
         }
+
+    def construct_response_node(self):
+        return [
+            { "mem_used": 500000 },
+            { "mem_used": 500000 }
+        ]
+
 if __name__ == "__main__":
     unittest.main()
