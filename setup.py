5a6269cd272a02dcee6852493ea96eb7769bdb8a
Kostas Milonas <milonas.ko@gmail.com>
2017-04-24 22:18:44 +0300

    excludes tests from setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #! /usr/bin/python2
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
-    name = "rabbitmq-alert",
-    packages = ["rabbitmqalert"],
-    version = "1.0",
-    description = "Send notifications when predefined conditions are met",
-    author = "Germano Fronza, Kostas Milonas",
-    author_email = "germano.inf@gmail.com",
-    url = "https://github.com/gfronza/rabbitmq-alert",
-    download_url = "https://github.com/gfronza/rabbitmq-alert/tarball/1.0",
-    keywords = ["rabbitmq", "alert", "monitor"],
-    classifiers = []
+    name="rabbitmq-alert",
+    packages=find_packages(exclude=["test*"]),
+    version="1.0.1",
+    description="Send notifications when predefined conditions are met",
+    author="Germano Fronza, Kostas Milonas",
+    author_email="germano.inf@gmail.com",
+    url="https://github.com/gfronza/rabbitmq-alert",
+    download_url="https://github.com/gfronza/rabbitmq-alert/tarball/1.0.1",
+    keywords=["rabbitmq", "alert", "monitor"],
+    classifiers=[]
 )
